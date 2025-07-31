df52257a4cf0f177e506536f1f423aa7b64748e8
LMAlieen <2277486684@qq.com>
2025-07-31 17:56:03 +0800

    change seqan_tcoffee x86 cmake cmd

--- recipes/seqan_tcoffee/build.sh
+++ recipes/seqan_tcoffee/build.sh
@@ -4,12 +4,7 @@ mkdir -p $PREFIX/bin
 
 mkdir -p build
 cd build
-if [[ ${target_platform}  == "linux-aarch64" ]]; then
-        cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_POLICY_VERSION_MINIMUM=3.5
-else
-        cmake .. -DCMAKE_BUILD_TYPE=Release
-fi
-
+cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_POLICY_VERSION_MINIMUM=3.5
 
 make seqan_tcoffee
 cp bin/seqan_tcoffee $PREFIX/bin
